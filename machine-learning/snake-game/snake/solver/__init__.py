475ac6289632a49753e866e1364202c37eca6bce
Justin Louise Neypes <38187304+jlrn-rph@users.noreply.github.com>
2020-11-01 17:49:27 +0800

    Snake module

---
+++ machine-learning/snake-game/snake/solver/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""AI solvers package."""
+
+from snake.solver.path import PathSolver
+from snake.solver.greedy import GreedySolver
+from snake.solver.hamilton import HamiltonSolver
+from snake.solver.dqn import DQNSolver
